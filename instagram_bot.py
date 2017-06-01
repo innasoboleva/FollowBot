e55384e56876df369cc53ee9394beaad7b109c07
Inna Soboleva <Inna.chugunova1@gmail.com>
2017-06-01 11:57:11 -0700

    Initial bot-follower for Instagram

---
+++ instagram_bot.py
@@ -0,0 +1,13 @@
+# PUT YOUR LOGIN AND PASSWORD
+# CHOOSE WHOSE FOLLOWERS YOU WANT TO FOLLOW
+# AND PUT THEM IN A FOLLOW LIST IN APOSTROPHES AND SEPARATE WITH COMMAS
+# HOW MANY PEOPLE TO FOLLOW PER DAY - BETTER NOT EXCEED 1000 OR CAN BE BANNED
+
+from functional import IgFollower
+
+follower = IgFollower(login="your_login",
+                 password="your_password",
+                 follow_list=['blogger-whose-followers-to-follow',
+                              'another-blogger',
+                              'one-more-blogger'],
+                 follow_per_day=500)
